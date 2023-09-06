fb6083f8db57097db3de4aaa57aed1d74af7d1e7
choodayul <dyulchoo@naver.com>
2023-09-06 17:10:52 +0900

    질문, 선택지 추가

--- js/choice3.js
+++ js/choice3.js
@@ -1,6 +1,6 @@
 let id = parseInt(new URL(location.href).searchParams.get('id'));
 
-const questionSpan = document.getElementsByClassName("question")[0].getElementsByTagName("span");
+const questionSpan = document.getElementsByClassName("question")[0].getElementsByTagName("span")[0];
 questionSpan.innerHTML = data.question[id].content;
 
 const choiceDiv = document.getElementsByClassName("choice")[0];
@@ -10,10 +10,13 @@ body.style.backgroundImage = `url('../image/bg1.png')`
 
 const choiceItem = document.getElementsByClassName('choice-item');
 
+// 추가함
 choiceItem[0].innerHTML = data.choice[id].content1; /*이런식으로*/
+choiceItem[1].innerHTML = data.choice[id].content2; /*이런식으로*/
+choiceItem[2].innerHTML = data.choice[id].content3; /*이런식으로*/
 
 for(let item of choiceItem) {
-    item.innerHTML = data.choice[id].content1;
+// 삭제함
     item.onclick = () => {
         id++;
         window.open(`choice3.html?id=${id}`, '_top');
