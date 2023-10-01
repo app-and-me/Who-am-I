47373d970b7cc7283bb3e35536c87e2873a10c11
hyeg0121 <hyeg0121@naver.com>
2023-10-01 17:42:57 +0900

    돌아가기 버튼 누르면 인덱스 화면으로

--- js/result.js
+++ js/result.js
@@ -27,7 +27,12 @@ const img = document.getElementsByTagName('img')[0];
 for (let i in data.characters) {
     if (data.characters[i].name === maxKey) {
         resultTitle.innerHTML = data.characters[i].name;
-        resultExplain.innerHTML = data.characters[i].explain;
+        resultExplain.innerHTML = '당신은 000 유형입니다. 이 유형의 학생은 두산 알칸타라 빨리 내려라 미친 정수빈 들어와서 기대했는데 뜬공이고 조수행도 병살이고 어휴 아시안 게임이나 보면서 힐링해야지 개같은거'+ data.characters[i].explain;
         img.src = data.characters[i].img;
     }
+}
+
+const restartButton = document.getElementsByClassName('restart')[0];
+restartButton.onclick = () => {
+    window.open('../index.html', '_top');
 }
