0b4c5834bd8526320cbace0fa3944486817371af
hyeg0121 <hyeg0121@naver.com>
2023-10-01 15:18:36 +0900

    최대값에 해당하는 캐릭터 이름, 설명, 이미지 넣기

--- js/result.js
+++ js/result.js
@@ -22,6 +22,12 @@ console.log("Key with Maximum Score:", maxKey);
 
 const resultTitle = document.getElementsByClassName('result-title')[0];
 const resultExplain = document.getElementsByClassName('result-explain')[0];
+const img = document.getElementsByTagName('img')[0];
 
-resultTitle.innerHTML = maxKey;
-resultExplain.innerHTML = '설명';
+for (let i in data.characters) {
+    if (data.characters[i].name === maxKey) {
+        resultTitle.innerHTML = data.characters[i].name;
+        resultExplain.innerHTML = data.characters[i].explain;
+        img.src = data.characters[i].img;
+    }
+}
