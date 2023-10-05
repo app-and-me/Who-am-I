2eddbadda4f06dac98c48ecf8ecefe641275a0eb
hyeg0121 <w2218@e-mirim.hs.kr>
2023-10-05 14:26:47 +0900

    결과 데이터 가져오기

--- js/result.js
+++ js/result.js
@@ -20,15 +20,40 @@ for (let i = 0; i < scores.length; i++) {
 console.log("Maximum Score:", maxScore);
 console.log("Key with Maximum Score:", maxKey);
 
+
+// 값 가져오기
+
 const resultTitle = document.getElementsByClassName('result-title')[0];
-const resultExplain = document.getElementsByClassName('result-explain')[0];
-const img = document.getElementsByClassName('character-img')[0];
+const resultDescriptionTitle = document.getElementsByClassName('result-description-title')[0];
+const resultContent = document.getElementsByClassName('result-content')[0];
+const resultTags = document.getElementsByClassName('result-tags')[0];
+const tagItems = [...document.getElementsByClassName('tag-item')];
+const resultPersonal = document.getElementsByClassName('result-personal')[0];
+const personalItems = [...document.getElementsByClassName('personal-item')];
+const characterImg = document.getElementsByClassName('character-img')[0];
+const goodContainer = document.getElementsByClassName('good-container')[0];
+const goodLable = goodContainer.getElementsByClassName('relation-lable')[0];
+const goodTypeImg = goodContainer.getElementsByClassName('type-img')[0];
+const goodTypeTitle = goodContainer.getElementsByClassName('type-title')[0];
+const badContainer = document.getElementsByClassName('bad-container')[0];
+const badLable = badContainer.getElementsByClassName('relation-lable')[0];
+const badTypeImg = badContainer.getElementsByClassName('type-img')[0];
+const badTypeTitle = badContainer.getElementsByClassName('type-title')[0];
 
 for (let i in data.characters) {
     if (data.characters[i].name === maxKey) {
-        resultTitle.innerHTML = data.characters[i].name;
-        resultExplain.innerHTML = data.characters[i].explain;
-        img.src = data.characters[i].img;
+        let character = data.characters[i];
+        resultTitle.innerHTML = character.name;
+        // resultExplainContainer.innerHTML = data.characters[i].explain.title;
+        characterImg.src = data.characters[i].img;
+        resultDescriptionTitle.innerHTML = character.description.title;
+        resultContent.innerHTML = character.description.content;
+        tagItems.forEach((e, index) => {
+            e.innerHTML = '#' + character.description.tag[index];
+        });
+        personalItems.forEach((e, index) => {
+            e.innerHTML = '- ' + character.description.personal[index]; 
+        });
     }
 }
 
